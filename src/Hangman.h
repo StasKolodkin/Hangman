4b671a7bf1f4a7394f499b65e27bad50685afff9
StasKolodkin <kolodkinstas@icloud.com>
2022-03-28 03:25:56 +0700

    Write a function to initialize the class responsible for the logic of the game

--- src/Hangman.h
+++ src/Hangman.h
@@ -2,11 +2,36 @@
 #define HANGMAN_H
 
 #include "entity/HangmanState.h"
+#include <algorithm>
+#include <stdexcept>
+#include <string>
 
 class Hangman {
 private:
     std::string word;
     HangmanState state;
+
+    void validateWord(const std::string& wd)
+    {
+        if (wd.empty())
+            throw std::runtime_error("Word is empty!");
+        for (u_long i = 0; i < wd.length(); ++i)
+            if (toupper(wd[i]) < 'A' || toupper(wd[i]) > 'Z')
+                throw std::runtime_error(
+                        "The word contains characters that are not letters!");
+    }
+
+public:
+    HangmanState start(const std::string& wd)
+    {
+        validateWord(wd);
+        word = wd;
+        state.lives = 6;
+        state.guessedLetters.clear();
+        state.hiddenWord = std::string(word.length(), '_');
+        std::transform(word.begin(), word.end(), word.begin(), ::toupper);
+        return state;
+    }
 };
 
 #endif
