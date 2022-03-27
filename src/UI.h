2a3254fe9f4c0e4edfdf3075ce6efa3455622b59
StasKolodkin <kolodkinstas@icloud.com>
2022-03-28 06:17:29 +0700

    Add screen cleaning feature

--- src/UI.h
+++ src/UI.h
@@ -2,15 +2,21 @@
 #define UI_H
 
 #include "Hangman.h"
+#include <iostream>
 
 class UI {
 private:
     Hangman hangman;
+    void cleanScreen()
+    {
+        std::cout << "\033c";
+    }
 
 public:
     void show()
     {
         std::setbuf(stdout, nullptr);
+        cleanScreen();
     }
 };
 
