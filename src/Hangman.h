68b331456c7fef98771ca8fdcc477e573b2043b9
StasKolodkin <kolodkinstas@icloud.com>
2022-03-28 05:02:30 +0700

    Write the main game logic

--- src/Hangman.h
+++ src/Hangman.h
@@ -39,6 +39,26 @@ public:
         else
             return state.lives == 0;
     }
+    HangmanState checkLetter(char letter)
+    {
+        if (word.empty() || state.hiddenWord.empty() || state.lives == 0)
+            throw std::runtime_error("Game is not started!");
+        if (!(toupper(letter) >= 'A' && toupper(letter) <= 'Z'))
+            throw std::runtime_error("The received symbol is not a letter!");
+
+        letter = toupper(letter);
+        if (state.guessedLetters.find(letter) == std::string::npos) {
+            state.guessedLetters += letter;
+            if (word.find(letter) == std::string::npos) {
+                --state.lives;
+            } else {
+                for (unsigned short i = 0; i < word.length(); ++i)
+                    if (word[i] == letter)
+                        state.hiddenWord[i] = letter;
+            }
+        }
+        return state;
+    }
 };
 
 #endif
