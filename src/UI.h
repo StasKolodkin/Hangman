f26918c18f44570d04c569903aa56580f07cc4f1
StasKolodkin <kolodkinstas@icloud.com>
2022-03-28 06:39:58 +0700

    Add game logo rendering

--- src/UI.h
+++ src/UI.h
@@ -21,11 +21,25 @@ private:
         std::cout << "\E[4" << color << "m";
     }
 
+    void drawLogo()
+    {
+        setForegroundColor(Colors::Cyan);
+        std::cout << " _\n"
+                     "| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __\n"
+                     "| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\\n"
+                     "| | | | (_| | | | | (_| | | | | | | (_| | | | |\n"
+                     "|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\n"
+                     "                   |___/";
+        std::cout << std::endl;
+        setForegroundColor(Colors::Reset);
+    }
+
 public:
     void show()
     {
         std::setbuf(stdout, nullptr);
         cleanScreen();
+        drawLogo();
     }
 };
 
