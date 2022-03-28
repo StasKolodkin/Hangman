0194086650868f3bd7265b7b6291d0b82ee3f9e6
StasKolodkin <kolodkinstas@icloud.com>
2022-03-28 07:54:00 +0700

    Create a function to get a random word from a set of words

--- src/WordHandler.h
+++ src/WordHandler.h
@@ -1,11 +1,25 @@
 #ifndef WORDHANLER_H
 #define WORDHANLER_H
 
+#include <random>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 class WordHandler {
+private:
     std::vector<std::string> words;
+
+public:
+    std::string getRandomWord()
+    {
+        if (words.empty())
+            throw std::runtime_error("No word base!");
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_int_distribution<u_long> distribution(0, words.size());
+        return words[distribution(generator)];
+    }
 };
 
 #endif
